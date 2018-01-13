cfe8e47c8f34758a6efef6834d168cf85ee2bdd8
Abid Hasan <sohailhsn@gmail.com>
2018-01-13 23:30:07 +0530

    initial commit

---
+++ public/sketch.js
@@ -0,0 +1,7 @@
+setup = () => {
+  createCanvas(200,200);
+}
+
+draw = () => {
+  rect(50,50,30,30);
+}
---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App2 extends React.Component {
+  render() {
+    return(
+      <div>
+      <p>hello</p>
+      </div>
+    );
+
+  }
+}
+
+ReactDOM.render(<App2 />, document.getElementById('root'));
